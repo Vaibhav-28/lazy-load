661d557e05635658b9db8935f78e781e2ec3a35b
rcl-vaibhav <vaibhav.sisodia@redcliffelabs.com>
2022-11-20 12:34:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import Card from './components/Card';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
+  const [posts,setPosts]  = useState(Array.from({length:5}))
+  const [hasMore,setHasMore] = useState(true)
+
+  useEffect(()=>{
+    console.log(posts,"<===")
+  },[posts])
+
+  const fetchMore = () => {
+    if(posts.length<=95){
+      setPosts(posts.concat(Array.from({length:5})))
+    }else{
+      setHasMore(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="head">Posts</div>
+        <InfiniteScroll dataLength={posts?.length} next={fetchMore} hasMore={hasMore} loader={<p>Loading...</p>}>
+          <div className="container">
+            {posts.length>0 && posts.map((post,i)=>{
+              return(
+                <div key={i} className="cards">
+                  <Card post={i}/>
+                </div>
+              )
+            })}
+          </div>
+        </InfiniteScroll>
+    </>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React, {useEffect,useState} from 'react'
+import './Card.css'
+
+function Card(props) {
+  const [post,setPost]  = useState([])
+  const fetchData = () =>{
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${props.post+1}`)
+    .then((response) => response.json())
+    .then((data) => setPost(data));
+  }
+  useEffect(() => {
+    console.log(props)
+    fetchData();
+  },[])
+  return (
+    <div>
+      <div className="card">
+        <div className="cardPic">
+          <img src={`https://avatars.dicebear.com/api/human/${post.userId}.svg`} alt="userPic" />
+        </div>
+        <div className="title">
+          {post.title}
+        </div>
+        <div className="desc">
+          {post?.body?.length > 70 ? post?.body.substring(0, 67) + "..." : post?.body}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
